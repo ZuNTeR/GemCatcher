1f79fd1f9a1ef0af718eefef28694ebc85d1d36a
Zunter <zunterr@mail.ru>
2024-09-25 15:45:37 -0300

    Mudanças nas bordas

--- scenes/GemScenes/gem.gd
+++ scenes/GemScenes/gem.gd
@@ -13,8 +13,14 @@ func _process(delta: float) -> void:
 	position.y += speed * delta
 	
 	
-	if position.y > get_viewport_rect().size.y + 25.0:
-		on_gem_off_screen.emit()
-		set_process(false)
-		queue_free()
-		set_process(true)
+#	if position.y > get_viewport_rect().size.y + 25.0:
+#		on_gem_off_screen.emit()
+#		set_process(false)
+#		queue_free()
+#		set_process(true)
+func _on_paredebaixo_area_entered(area: Area2D) -> void:
+	on_gem_off_screen.emit()
+	set_process(false)
+	queue_free()
+	set_process(true)
+	print 
--- scenes/game.gd
+++ scenes/game.gd
@@ -6,6 +6,9 @@ extends Node2D
 @onready var audio_gema_capturada: AudioStreamPlayer2D = $AudioGemaCapturada
 @onready var audio_gema_perdida: AudioStreamPlayer2D = $AudioGemaPerdida
 
+
+
+
 #signal on_gem_touch_under_bound
 
 var _score: int = 0
@@ -20,29 +23,41 @@ func _ready() -> void:
 func spawn_gem() -> void:
 	var new_gem = gem_scene.instantiate()
 	var xpos: float = randf_range(70, 1050)
-	new_gem.on_gem_off_screen.connect(lose_gem)
+	#new_gem.on_gem_off_screen.connect(lose_gem)
 	new_gem.position = Vector2(xpos, -50)
 	add_child(new_gem)
 
-func stop_all() -> void:
-	timer.stop()
+#func stop_all() -> void:
+#	timer.stop()
 
 func _on_timer_timeout() -> void:
-	print("_on_timer_timeout")
 	spawn_gem()
 	
-func lose_gem() -> void:
-	_score -= 1
-	label.text = "%05d" % _score
-	audio_gema_perdida.play()
-	if _score < 0:
-		stop_all()
+#func lose_gem() -> void:
+#	_score -= 1
+#	label.text = "%05d" % _score
+#	audio_gema_perdida.play()
+#	if _score < 0:
+#		stop_all()
 
 func _on_paddle_area_entered(area: Area2D) -> void:
 	_score += 1
 	label.text = "%05d" % _score
 	audio_gema_capturada.play()
 	area.queue_free()
-	
-	#func _p
-	
+
+
+func _on_parede_baixo_area_entered(area: Area2D) -> void:
+	pass
+
+
+func _on_parede_direita_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_parede_esquerda_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_parede_cima_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
